fd32c74f85559d2a5a369e863d62da6c79d5be39
LandisDuffett <ldavduff@yahoo.com>
2022-05-05 18:59:31 -0700

    now complete

--- src/presentation/ProductManagementSystem.java
+++ src/presentation/ProductManagementSystem.java
@@ -41,13 +41,92 @@ public class ProductManagementSystem {
 					proceed = scan.next().charAt(0);
 					break;
 				case 2:
-					
+					ProductPojo Prod = new ProductPojo();
+					System.out.println("***************************************************************************************");
+					System.out.println("Enter product name:");
+					String prodName = scan.next();
+					Prod.setProductName(prodName);
+					System.out.println("Enter product description: ");
+					scan.nextLine();
+					String prodDescr = scan.nextLine();
+					Prod.setProductDescription(prodDescr);
+					System.out.println("Enter product price:");
+					int prodCost = scan.nextInt();
+					Prod.setProductCost(prodCost);
+					System.out.println("Enter product Image URL:");
+					String prodImage = scan.next();
+					Prod.setProductImageUrl(prodImage);
+					productService.addProduct(Prod);
+					System.out.println("***************************************************************************************");
+					System.out.println("Do you want to continue?(y/n)");
+					proceed = scan.next().charAt(0);
 					break;
 				case 3:
-					
+					List<ProductPojo> allProducts = productService.getAllProducts();
+					System.out.println("***************************************************************************************");
+					System.out.println("ID\tNAME");
+					System.out.println("***************************************************************************************");
+					allProducts.forEach((item)-> System.out.println(item.getProductId() + "\t" + item.getProductName()));
+					System.out.println("***************************************************************************************");
+					System.out.println("Enter the ID of the product you would like to delete: ");
+					int delItem = scan.nextInt();
+					productService.deleteProduct(delItem);
+					System.out.println("***************************************************************************************");
+					System.out.println("Do you want to continue?(y/n)");
+					proceed = scan.next().charAt(0);
 					break;
 				case 4:
-					
+					List<ProductPojo> omniProducts = productService.getAllProducts();
+					System.out.println("***************************************************************************************");
+					System.out.println("ID\tNAME");
+					System.out.println("***************************************************************************************");
+					omniProducts.forEach((item)-> System.out.println(item.getProductId() + "\t" + item.getProductName()));
+					System.out.println("***************************************************************************************");
+					System.out.println("Enter the ID of the product you would like to edit: ");
+					int edItem = scan.nextInt();
+					ProductPojo edProd = productService.getAProduct(edItem);
+					System.out.println("*****************************");
+					System.out.println("1. Edit product name");
+					System.out.println("2. Edit product description");
+					System.out.println("3. Edit product price");
+					System.out.println("4. Edit product Image URL");
+					System.out.println("*****************************");
+					System.out.println("Please enter an option:");
+					int choice = scan.nextInt();
+					switch(choice) {
+						case 1:
+							System.out.println("current name is: " + edProd.getProductName() );
+							System.out.println("enter new name: ");
+							String newName = scan.next();
+							edProd.setProductName(newName);
+							productService.updateProduct(edProd);
+							break;
+						case 2:
+							System.out.println("current description is: " + edProd.getProductDescription() );
+							System.out.println("enter new description:");
+							scan.nextLine();
+							String newDescr = scan.nextLine();
+							edProd.setProductDescription(newDescr);
+							productService.updateProduct(edProd);
+							break;
+						case 3:
+							System.out.println("current price is: " + edProd.getProductCost() );
+							System.out.println("enter new price:");
+							int newPrice = scan.nextInt();
+							edProd.setProductCost(newPrice);
+							productService.updateProduct(edProd);
+							break;
+						case 4:
+							System.out.println("current image URL is: " + edProd.getProductImageUrl() );
+							System.out.println("enter new image URL:");
+							String newURL = scan.next();
+							edProd.setProductImageUrl(newURL);
+							productService.updateProduct(edProd);
+							break;
+					}
+					System.out.println("****************************************************************************************");
+					System.out.println("Do you want to continue?(y/n)");
+					proceed = scan.next().charAt(0);
 					break;
 				case 5:
 					System.out.println("***************************************************************************************");
